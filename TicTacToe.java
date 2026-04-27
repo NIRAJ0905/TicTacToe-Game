b55e87db5f8571cbb1a3b0bcc218506566f49df2
NIRAJ0905 <nirajpingale005@gmail.com>
2026-04-27 18:53:30 +0530

    Completed UC2 Toss and Symbol Assignment

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,17 +1,22 @@
+import java.util.Random;
+
 public class TicTacToe {
 
-    // 3x3 board created
     static char[][] board = new char[3][3];
 
-    // Main method starts program
+    static char playerSymbol;
+    static char computerSymbol;
+    static String currentTurn;
+
     public static void main(String[] args) {
 
-        initializeBoard();   // fill board with '-'
+        initializeBoard();
 
-        printBoard();        // display board
+        toss();
+
+        printBoard();
     }
 
-    // Fill all cells with '-'
     static void initializeBoard() {
 
         for (int row = 0; row < 3; row++) {
@@ -19,14 +24,36 @@ public class TicTacToe {
             for (int col = 0; col < 3; col++) {
 
                 board[row][col] = '-';
-
             }
-
         }
-
     }
 
-    // Print board properly
+    static void toss() {
+
+        Random random = new Random();
+
+        int result = random.nextInt(2);   // 0 or 1
+
+        if (result == 0) {
+
+            System.out.println("Player won toss!");
+            playerSymbol = 'X';
+            computerSymbol = 'O';
+            currentTurn = "Player";
+
+        } else {
+
+            System.out.println("Computer won toss!");
+            playerSymbol = 'O';
+            computerSymbol = 'X';
+            currentTurn = "Computer";
+        }
+
+        System.out.println("Player Symbol: " + playerSymbol);
+        System.out.println("Computer Symbol: " + computerSymbol);
+        System.out.println("First Turn: " + currentTurn);
+    }
+
     static void printBoard() {
 
         System.out.println("-------------");
@@ -36,14 +63,10 @@ public class TicTacToe {
             for (int col = 0; col < 3; col++) {
 
                 System.out.print("| " + board[row][col] + " ");
-
             }
 
             System.out.println("|");
             System.out.println("-------------");
-
         }
-
     }
-
 }
