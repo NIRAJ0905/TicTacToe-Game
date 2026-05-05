7e26f49fb9aaffbf5c2c55a41b6fc6e916d96853
NIRAJ0905 <nirajpingale005@gmail.com>
2026-05-05 23:17:22 +0530

    Completed UC7 Computer Random Move

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,17 +1,72 @@
+import java.util.Random;
+
 public class TicTacToe {
 
-    static char[][] board = new char[3][3];
+    static char[][] board = {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'}
+    };
+
+    static char computerSymbol = 'O';
 
     public static void main(String[] args) {
 
-        placeMove(0, 0, 'X');
+        computerMove();
 
-        System.out.println(board[0][0]);
+        printBoard();
+    }
+
+    static void computerMove() {
+
+        Random random = new Random();
+
+        while (true) {
+
+            int slot = random.nextInt(9) + 1;  // 1 to 9
+
+            int row = getRowFromSlot(slot);
+            int col = getColFromSlot(slot);
+
+            if (isValidMove(row, col)) {
+
+                placeMove(row, col, computerSymbol);
+                break;
+            }
+        }
+    }
+
+    static int getRowFromSlot(int slot) {
+        return (slot - 1) / 3;
+    }
+
+    static int getColFromSlot(int slot) {
+        return (slot - 1) % 3;
+    }
+
+    static boolean isValidMove(int row, int col) {
+        return (row >= 0 && row <= 2 &&
+                col >= 0 && col <= 2 &&
+                board[row][col] == '-');
     }
 
     static void placeMove(int row, int col, char symbol) {
-
         board[row][col] = symbol;
+    }
 
+    static void printBoard() {
+
+        System.out.println("-------------");
+
+        for (int i = 0; i < 3; i++) {
+
+            for (int j = 0; j < 3; j++) {
+
+                System.out.print("| " + board[i][j] + " ");
+            }
+
+            System.out.println("|");
+            System.out.println("-------------");
+        }
     }
 }
