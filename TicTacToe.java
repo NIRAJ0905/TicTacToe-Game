a24633e9884dc7982fd701d37fdb44e15c878d3b
NIRAJ0905 <nirajpingale005@gmail.com>
2026-05-05 23:14:50 +0530

    Completed UC6 Place Move on Board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,26 +1,17 @@
 public class TicTacToe {
 
-    static char[][] board = {
-        {'-', '-', '-'},
-        {'-', '-', '-'},
-        {'-', '-', '-'}
-    };
+    static char[][] board = new char[3][3];
 
     public static void main(String[] args) {
 
-        System.out.println(isValidMove(1, 1));
+        placeMove(0, 0, 'X');
 
+        System.out.println(board[0][0]);
     }
 
-    static boolean isValidMove(int row, int col) {
+    static void placeMove(int row, int col, char symbol) {
 
-        if (row >= 0 && row <= 2 &&
-            col >= 0 && col <= 2 &&
-            board[row][col] == '-') {
+        board[row][col] = symbol;
 
-            return true;
-        }
-
-        return false;
     }
 }
